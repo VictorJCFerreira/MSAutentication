dd976e612f1c718bce05cd91ca4c91db061f1ff5
Victor Ferreira <victorjcferreira462@gmail.com>
2023-01-09 18:16:05 -0300

    Post e Put

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express, {Request, Response, NextFunction } from "express"
+import usersRouter from "./routes/users.route";
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(usersRouter);
 
 app.get('/status', (req: Request,res: Response,next: NextFunction )=> {
     res.status(200).send({ foo: "PRA CIMA DELES!!!" })
---
+++ src/routes/users.route.ts
@@ -0,0 +1,35 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import { before } from "node:test";
+
+const usersRouter = Router()
+
+usersRouter.get("/users", (req: Request, res: Response, next: NextFunction)=>{
+    const users = [{username: "Victor",}];
+    res.status(StatusCodes.OK).json(users);
+
+})
+
+usersRouter.get("/users/:uuid",(req: Request<{ uuid: String }>, res: Response, next: NextFunction)=>{
+    const uuid = req.params.uuid
+    //BancoDeDados.getUserByUuid(uuid)
+    res.status(StatusCodes.OK).send({uuid})
+})
+
+usersRouter.post("/users",(req: Request<{ uuid: String }>, res: Response, next: NextFunction)=>{
+    const newUser = req.body
+
+    res.status(StatusCodes.CREATED).send(newUser)
+})
+
+usersRouter.put("/users/:uuid",(req: Request<{ uuid: String }>, res: Response, next: NextFunction)=>{
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body
+
+    modifiedUser.uuid = uuid
+
+    res.status(StatusCodes.OK).send( modifiedUser )
+} )
+
+export default usersRouter;
+
