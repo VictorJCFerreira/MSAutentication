7e95545dc5cdf8776dc59a01e6609a82e8cd6445
Victor Ferreira <victorjcferreira462@gmail.com>
2023-01-09 16:33:02 -0300

    Add TS e Automação

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Dale rapaziada")
+import express, {Request, Response, NextFunction } from "express"
+
+const app = express();
+
+app.get('/status', (req: Request,res: Response,next: NextFunction )=> {
+    res.status(200).send({ foo: "PRA CIMA DELES!!!" })
+})
+
+app.listen(3000, () => {
+    console.log("Excecução na porta 3000!")
+})
