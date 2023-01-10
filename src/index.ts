13d270a37e72e2d1ca98ad215b4d5e4ee712d71c
Victor Ferreira <victorjcferreira462@gmail.com>
2023-01-10 19:32:45 -0300

    ErrorHanddler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response, NextFunction } from "express"
+import errorHandler from "./middleware/error-handler.middleware";
 import statusRouter from "./routes/status.route";
 import usersRouter from "./routes/users.route";
 
@@ -9,6 +10,8 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(usersRouter)
 app.use(statusRouter)
+
+app.use(errorHandler)
 
 app.listen(3000, () => {
     console.log("Excecução na porta 3000!")
